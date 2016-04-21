f00c402c441191eff8eb784e1caada073683123b
Attila Györffy <attila@attilagyorffy.com>
2016-04-21 15:58:54 +0200

    Fix recursive loop in setInterval lol

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
   function setNowplaying() {
-    $.getJSON('/nowplaying', function(data) {
-      $('#nowplaying').html(data.current_track);
-    })
-
-    setInterval(setNowplaying, 60000);
+    try {
+      $.getJSON('/nowplaying', function(data) {
+        console.log('Updated nowplaying: %s', data.current_track);
+        $('#nowplaying').html(data.current_track);
+      });
+    }
+    // in case the browser does not have a console available.
+    catch (e) {
+      console.log(e);
+    }
   }
 
   setNowplaying();
+  setInterval(setNowplaying, 60000);
 });
