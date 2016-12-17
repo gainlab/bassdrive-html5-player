4af35da58e033da7f50c473fe601496a7dc76ec6
Attila Györffy <attila@attilagyorffy.com>
2016-12-17 14:15:00 +0200

    Use original bassdrive styling for easier migration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,5 @@
+require 'nokogiri'
+
 module ApplicationHelper
   def content_type_for_relay(relay)
     case relay['audioFormat']
@@ -7,4 +9,33 @@ module ApplicationHelper
       'audio/aacp'
     end
   end
+
+  def svg_image_tag(filename, options = {})
+    svg = markup_for_svg_image_file(filename).at_css 'svg'
+    # Ensure preserveAspectRatio is always set so that SVGs align to top left.
+    svg['preserveAspectRatio'] = 'xMinYMin'
+
+    %w(xmlns xmlns:xlink xml:space width height style version).each { |attribute| svg.delete attribute }
+
+    if options[:class]
+      if svg['class'].present?
+        svg['class'] << " #{options[:class]}"
+      else
+        svg['class'] = options[:class]
+      end
+    end
+
+    svg.to_html.html_safe
+  end
+
+private
+
+  def svg_image_file(filename)
+    File.read Rails.root.join('app', 'assets', 'images', filename)
+  end
+
+  def markup_for_svg_image_file(filename)
+    Nokogiri::HTML::DocumentFragment.parse(svg_image_file(filename))
+  end
+
 end
