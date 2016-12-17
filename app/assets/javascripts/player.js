afa52cbd40fb3e44e33dd32fe806f238b3c18f25
Attila Györffy <attila@attilagyorffy.com>
2016-12-17 15:29:27 +0200

    Style custom controls

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     try {
       $.getJSON('/nowplaying', function(data) {
         console.log('Updated nowplaying: %s', data.current_track);
-        $('#nowplaying').html(data.current_track);
+        $('.js-playing-now').html(data.current_track);
       });
     }
     // in case the browser does not have a console available.
